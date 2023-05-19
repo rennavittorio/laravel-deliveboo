711f044ad2ce2be2b3fb24168ec4188c57e7c09a
fracs31 <pist.fracs95@gmail.com>
2023-05-19 10:21:45 +0200

    ADD: comment for the branch test

--- database/migrations/2023_05_18_144117_create_restaurants_table.php
+++ database/migrations/2023_05_18_144117_create_restaurants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
-
+            //Francesco che testa i branch
             $table->string('name', 100);
             $table->string('img', 255)->default('https://picsum.photos/200');
             $table->string('slug', 255)->unique();
