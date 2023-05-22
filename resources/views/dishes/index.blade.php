220a8aa437584195650f80e195e558e3314df4a3
rennavittorio <renna.vittorio@gmail.com>
2023-05-22 12:30:19 +0200

    add: add index view details fix: fix controller in order to get only auth-user related dishes

--- resources/views/dishes/index.blade.php
+++ resources/views/dishes/index.blade.php
@@ -4,7 +4,42 @@
 <div class="container">
 
 
-    siamo dentro INDEX, caro il nostro bel {{ $user->name }}
+    <h2 class="fs-4 text-secondary my-4">
+        Ciao {{ $user->name }}, ecco la lista dei tuoi piatti:
+    </h2>
+
+    <div class="grid">
+
+        
+        <a href="{{ route('dishes.create') }}">
+            <div class="card d-flex justify-content-center align-items-center">
+
+                <h1>
+                +
+                </h1>
+            
+            </div>
+        </a>
+
+        @foreach ($dishes as $dish)
+
+        <div class="card d-flex flex-column">
+            <img src="{{ $dish->img }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $dish->name }}</h5>
+                <p class="card-text">{{ $dish->description }}</p>
+                <div class="wrapper-footer-card d-flex justify-content-between align-items-center mt-auto">
+
+                    <p class="card-text badge m-0 bg-warning">{{ $dish->price }}</p>
+                    <p class="card-text badge bg-danger">{{ $dish->is_visible }}</p>
+
+                </div>
+            </div>
+        </div>
+            
+        @endforeach
+
+    </div>
 
 
     <a href="{{ route('dishes.create')}}" class="btn btn-primary m-3">create</a>
