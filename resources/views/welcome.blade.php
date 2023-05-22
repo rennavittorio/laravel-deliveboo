98ca2bbdd3291bfc8139ed7163e8ad0ecc9545bf
rennavittorio <renna.vittorio@gmail.com>
2023-05-22 22:52:55 +0200

    edit: edit welcome page style and add login/register btn to main view edit: edit HOME default route to homepage, and delete dashboard access (duplicate of homepage now)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,39 @@
 @section('content')
 
 
-<div class="container p-5">
+<div class="welcome container p-5">
 
-    loggati
+    
+    @guest
+    <div class="wrapper d-flex justify-content-center align-items-center gap-3">
+        <a class="nav-link" href="{{ route('login') }}">
+            <div class="card d-flex justify-content-center align-items-center">
+                {{ __('Login') }}
+            </div>
+        </a>
+        @if (Route::has('register'))
+        <a class="nav-link" href="{{ route('register') }}">
+            <div class="card d-flex justify-content-center align-items-center">
+                {{ __('Register') }}
+            </div>
+        </a>
+    </div>
+
+    @endif
+    @else
+    <h1 class="mt-5">
+        Benvenuto nella tua dashboard <span class="badge bg-warning">{{ Auth::user()->name }}</span>
+    </h1>
+
+    <ul class="list-group mt-3">
+        {{-- Piatti --}}
+        <li class="list-group-item"><a href="{{ route('dishes.index') }}">Visualizza i tuoi piatti</a></li>
+        {{-- Ordini --}}
+        <li class="list-group-item"><a href="">Visualizza i tuoi ordini</a></li>
+    </ul>
+    @endguest
+    
+
 
 </div>
 
