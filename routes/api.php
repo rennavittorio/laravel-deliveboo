1772cde813fb253887a83e74c6d67672a4ba4761
Lapo0 <lapo.gini.01@gmail.com>
2023-05-23 17:51:16 +0200

    feat api route && Api/RestaurantController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/restaurant', [RestaurantController::class, 'index']);
+
+Route::get('/restaurant/{slug}', [RestaurantController::class, 'show']);
