8622656fc391da0e486dc02ad0cdc72a7a6e9025
eSSeGH <ste.virgadaula@gmail.com>
2023-05-24 17:40:46 +0200

    FEATfetchByCategories controller query decommented

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class RestaurantController extends Controller
 {
     public function index() {
-        /*if (request()->has('categories')) {
+        if (request()->has('categories')) {
             $restaurants = Restaurant::join('category_restaurant', 'restaurants.id', '=', 'category_restaurant.restaurant_id')
                                         ->join('categories', 'categories.id', '=', 'category_restaurant.category_id')
                                         ->whereIn('categories.id', request()->query('categories'))
@@ -18,7 +18,7 @@ class RestaurantController extends Controller
                                         ->get('restaurants.*');
         } else {
             $restaurants = Restaurant::with('categories')->limit(20)->get();
-        }*/
+        }
         $restaurants = Restaurant::with('categories')->limit(20)->get();
         return response()->json([
             'success' => true,
