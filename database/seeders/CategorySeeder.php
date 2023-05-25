e132e04388973469762a7d65a8575cf13525e872
Lapo0 <lapo.gini.01@gmail.com>
2023-05-25 10:12:26 +0200

    feat category image

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -17,18 +17,54 @@ class CategorySeeder extends Seeder
     {
         //Categorie
         $categories = [
-            'Italiano', 
-            'Francese',
-            'Cinese', 
-            'Giapponese', 
-            'Indiano', 
-            'Messicano',
-            'Argentino', 
-            'Internazionale', 
-            'Pesce', 
-            'Carne', 
-            'Pizza',
-            'Sushi'
+            [
+                'name' => 'Italiano',
+                'image' => 'public/storage/food-categories/italiano.jpg'
+            ],
+            [
+                'name' => 'Francese',
+                'image' => 'public/storage/food-categories/francese.jpg'
+            ],
+            [
+                'name' => 'Cinese',
+                'image' => 'public/storage/food-categories/cinese.jpg'
+            ],
+            [
+                'name' => 'Giapponese',
+                'image' => 'public/storage/food-categories/giapponese.jpg'
+            ],
+            [
+                'name' => 'Indiano',
+                'image' => 'public/storage/food-categories/indiano.jpg'
+            ],
+            [
+                'name' => 'Messicano',
+                'image' => 'public/storage/food-categories/messicano.jpg'
+            ],
+            [
+                'name' => 'Argentino',
+                'image' => 'public/storage/food-categories/argentina.jpg'
+            ],
+            [
+                'name' => 'Internazionale',
+                'image' => 'public/storage/food-categories/internazionale.jpg'
+            ],
+            [
+                'name' => 'Pesce',
+                'image' => 'public/storage/food-categories/pesce.jpg'
+            ],
+            [
+                'name' => 'Carne',
+                'image' => 'public/storage/food-categories/carne.jpg'
+            ],
+            [
+                'name' => 'Pizza',
+                'image' => 'public/storage/food-categories/pizza.jpg'
+            ],
+            [
+                'name' => 'Sushi',
+                'image' => 'public/storage/food-categories/sushi.jpg'
+            ]
         ];
         //Ciclo
         foreach($categories as $category) {
