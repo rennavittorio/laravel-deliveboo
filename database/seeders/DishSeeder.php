3d09743463dbfa193d9d125aa1463ca42f79c312
fracs31 <pist.fracs95@gmail.com>
2023-05-25 15:02:33 +0200

    ADD: more restaurants, dishes and categories

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -99,6 +99,86 @@ class DishSeeder extends Seeder
                 'is_visible' => 1,
                 'restaurant_id' => 5
             ],
+            [
+                'name' => 'Plin di carne e zucca al sugo d’arrosto e amaretto',
+                'img' => 'plin.jpg',
+                'description' => 'Plin di carne e zucca al sugo d’arrosto e amaretto',
+                'price' => 15,
+                'is_visible' => 1,
+                'restaurant_id' => 6
+            ],
+            [
+                'name' => 'Tajarin burro e salvia',
+                'img' => 'tajarin.jpg',
+                'description' => 'Tajarin con burro e salvia',
+                'price' => 15,
+                'is_visible' => 1,
+                'restaurant_id' => 6
+            ],
+            [
+                'name' => 'Special Box 32pz',
+                'img' => 'special_box.jpg',
+                'description' => '(2 uramaki special, 2 uramaki regular) Astice gratinato al mango, Carnaval, tonno e pistacchio, salmone alla fiamma',
+                'price' => 60,
+                'is_visible' => 1,
+                'restaurant_id' => 7
+            ],
+            [
+                'name' => 'Mix Box 32pz',
+                'img' => 'mix_box.jpg',
+                'description' => '(1 uramaki special, 3 uramaki regular) Sweet & sour salmon, Astice gratinato, Dragon Messicano, Extra spicy tonno',
+                'price' => 50,
+                'is_visible' => 1,
+                'restaurant_id' => 7
+            ],
+            [
+                'name' => 'Kebab',
+                'img' => 'kebab.jpg',
+                'description' => 'Kebab',
+                'price' => 5.50,
+                'is_visible' => 1,
+                'restaurant_id' => 8
+            ],
+            [
+                'name' => 'Pizza Kebab',
+                'img' => 'pizza_kebab.jpg',
+                'description' => 'Pizza con il kebab',
+                'price' => 7.50,
+                'is_visible' => 1,
+                'restaurant_id' => 8
+            ],
+            [
+                'name' => 'Cheese Naan',
+                'img' => 'cheese_naan.jpg',
+                'description' => 'Il classico pane indiano di farina bianca ripieno di formaggio',
+                'price' => 3.50,
+                'is_visible' => 1,
+                'restaurant_id' => 9
+            ],
+            [
+                'name' => 'Fish Pakora',
+                'img' => 'fish_pakora.jpg',
+                'description' => 'Bocconcini di pesce spada marinato con lime, zenzero fresco e spezie',
+                'price' => 3.50,
+                'is_visible' => 1,
+                'restaurant_id' => 9
+            ],
+            [
+                'name' => 'Steak Tartare',
+                'img' => 'steak_tartare.jpg',
+                'description' => 'Filetto di Angus Irlandese battuto al coltello & condito. Servito con maionese alla senape e tuorlo d\'uovo bio',
+                'price' => 14.50,
+                'is_visible' => 1,
+                'restaurant_id' => 10
+            ],
+            [
+                'name' => 'Fish & Chips',
+                'img' => 'fish_and_chips.jpg',
+                'description' => 'Filetto di merluzzo nella tradizionale pastella di birra rossa. Servito patate fritte.',
+                'price' => 20,
+                'is_visible' => 1,
+                'restaurant_id' => 10
+            ],
         ];
         //Ciclo
         foreach ($dishes as $dish) {
