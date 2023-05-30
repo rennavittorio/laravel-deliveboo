edf3a8b81386745614f3fe3428e238c4ceeb7b9c
fracs31 <pist.fracs95@gmail.com>
2023-05-30 13:11:55 +0200

    ADD: operation to get the final price of the order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -12,22 +12,31 @@ class OrderController extends Controller
     public function store(Request $request)
     {
 
-        $newOrder = new Order();
-        $newOrder->total = $request->totalCart;
-        $newOrder->status = '0'; //stato del pagamento
-        $newOrder->first_name = $request->form['firstName'];
-        $newOrder->last_name = $request->form['lastName'];
-        $newOrder->email = $request->form['email'];
-        $newOrder->phone = $request->form['phone'];
-        $newOrder->address = $request->form['address'];
-        $newOrder->postal_code = $request->form['postalCode'];
-        $newOrder->save();
+        $amount = 0; //prezzo finale
 
+        //Calcolo il prezzo finale
         foreach ($request->cart as $dish) {
+            $amount += $dish['price'] * $dish['quantity'];
+        }
+
+        $newOrder = new Order(); //nuovo ordine
+        $newOrder->total = $amount; //prezzo finale dell'ordine
+        $newOrder->status = 0; //stato del pagamento
+        $newOrder->first_name = $request->form['firstName']; //nome del cliente
+        $newOrder->last_name = $request->form['lastName']; //cognome del cliente
+        $newOrder->email = $request->form['email']; //email del cliente
+        $newOrder->phone = $request->form['phone']; //telefono del cliente
+        $newOrder->address = $request->form['address']; //indirizzo del cliente
+        $newOrder->postal_code = $request->form['postalCode']; //codice postale del cliente
+        $newOrder->save(); //invio i dati dal database
+        
+        //Inserisco i dati nella tabella ponte tra gli ordini e i piatti
+        foreach ($request->cart as $dish) {
+            //Inserimento nel database
             DB::table('dish_order')->insert([
-                'dish_id' => $dish['id'],
-                'order_id' => $newOrder->id,
-                'quantity' => $dish['quantity']
+                'dish_id' => $dish['id'], //id del piatto
+                'order_id' => $newOrder->id, //id dell'ordine
+                'quantity' => $dish['quantity'] //quantità del piatto ordinato
             ]);
         }
 
