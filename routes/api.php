ff2d0b673249ae8fd1647cbb368c96642f81509a
fracs31 <pist.fracs95@gmail.com>
2023-05-30 22:18:11 +0200

    ADD: Lead's route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderController; //ordine
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\RestaurantController; //ristorante
+use App\Http\Controllers\Api\LeadController; //lead
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,8 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Ristorante
 Route::get('/restaurants', [RestaurantController::class, 'index']);
-
 Route::get('/restaurants/{slug}', [RestaurantController::class, 'show']);
 
+//Pagamento
 Route::post('/order/pay', [OrderController::class, 'store']);
+
+//Mail
+Route::post('/leads', [LeadController::class, 'store']);
