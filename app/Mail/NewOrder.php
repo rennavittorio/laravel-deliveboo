fe7c808178bad4fc497f00c1ba41b7296c679b0a
fracs31 <pist.fracs95@gmail.com>
2023-05-30 21:26:34 +0200

    CHANGE: mail name

--- app/Mail/NewOrder.php
+++ app/Mail/NewOrder.php
@@ -9,18 +9,20 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewContact extends Mailable
+class NewOrder extends Mailable
 {
     use Queueable, SerializesModels;
+
+    public $lead;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($_lead)
     {
-        //
+        $this->lead = $_lead;
     }
 
     /**
@@ -31,7 +33,8 @@ class NewContact extends Mailable
     public function envelope()
     {
         return new Envelope(
-            subject: 'New Contact',
+            replyTo: $this->lead->address,
+            subject: 'Nuovo ordine',
         );
     }
 
@@ -43,7 +46,7 @@ class NewContact extends Mailable
     public function content()
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.new-order-mail',
         );
     }
 
