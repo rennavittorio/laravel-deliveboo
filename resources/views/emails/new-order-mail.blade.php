791129d64730fe4ded0e3989e1dd3e23c108704b
fracs31 <pist.fracs95@gmail.com>
2023-06-01 10:25:21 +0200

    ADD: style to the email template

--- resources/views/emails/new-order-mail.blade.php
+++ resources/views/emails/new-order-mail.blade.php
@@ -1,6 +1,34 @@
+<style>
+	h1 {
+  		font-weight: normal;
+  	}
+  	table {
+  		width: 100%;
+  		text-align: left;
+  		border-collapse: collapse;
+  	}
+  	th {
+  		background-color:#9D4EDD;
+  		color:white;
+  		font-size: 18px;
+  		font-weight: normal;
+  		padding: 5px;
+  	}
+  	td {
+  		padding: 5px;
+  	}
+    tr:nth-child(even) {
+  		background-color: #FAF9F9;
+  	}
+</style>
+
 <h1>
     Nuovo ordine!
 </h1>
+
+<p>
+    {{ $lead->message['message'] }}
+</p>
 
 <table>
     <thead>
@@ -14,21 +42,38 @@
             <th>
                 Quantità
             </th>
+            <th>
+                Prezzo totale
+            </th>
         </tr>
     </thead>
     <tbody>
-        @foreach ($lead->message as $dish)
+        @foreach ($lead->message['cart'] as $dish)
             <tr>
                 <td>
                     {{ $dish['name'] }}
                 </td>
                 <td>
-                    {{ $dish['price'] }}
+                    {{ $dish['price'] }}€
                 </td>
                 <td>
                     {{ $dish['quantity'] }}
+                </td>
+                <td>
+                    {{ $dish['price'] * $dish['quantity'] }}€
                 </td>
             </tr>
         @endforeach
     </tbody>
 </table>
+
+<?php
+    $total = 0;
+    foreach ($lead->message['cart'] as $dish) {
+        $total += $dish['price'] * $dish['quantity'];
+    }
+?>
+
+<p>
+    Spesa totale: {{ $total }}€
+</p>
