900b38c7149c389f242344f2baf09df2e762a373
fracs31 <pist.fracs95@gmail.com>
2023-06-01 16:30:40 +0200

    ADD: Chart.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,32 @@
             </div>
         </div>
     </div>
-
+    {{-- Statistiche --}}
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            scales: {
+                y: {
+                beginAtZero: true
+                }
+            }
+            }
+        });
+    </script>
     {{-- <ul class="list-group mt-3">
         <li class="list-group-item"><a href="{{ route('dishes.index') }}">Visualizza i tuoi piatti</a></li>
         <li class="list-group-item"><a href="">Visualizza i tuoi ordini</a></li>
